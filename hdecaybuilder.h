b188937b8c8732502e7921f35624d9f2a2d44eb0
Ubuntu MATE <ubuntumate@ubuntumate-1804.linuxvmimages.local>
2021-05-10 09:42:18 -0400

    First developments of decay builder

--- hdecaybuilder.h
+++ hdecaybuilder.h
@@ -28,13 +28,47 @@ private:
     std::vector<HRefitCand> fCands;
     int fVerbose;
 
+    // Variables used for setting the covariance matrix
+    bool fFixedErrors;
+
+    // Containers for the protons, kaons and pions in the event
+    std::vector<HRefitCand> fProtons;
+    std::vector<HRefitCand> fPions;
+    std::vector<HRefitCand> fKaons;
+
+    // Variables used for the vertex finding
+    bool fFindPrimaryVertex;
+    bool fFindDecayVertex;
+
+    // Variables used for setting the covariance matrix
+    bool fFixedErrors;
+    bool fMomDepErrors;
+
+    std::vector<HRefitCand> 
+
 public:
     HDecayBuilder();
     ~HDecayBuilder(){};
 
     void setVerbosity(int val) { fVerbose = val; }
 
+    // Method to fill the data from a HRefitCand for simulations
     void FillData(HParticleCand * cand, HRefitCand & outcand, double arr[], double mass);
+
+    // Method to fill the data from a KParticleCand from old data
+    void FillData(KParticleCand * cand, HRefitCand & outcand, double arr[], double mass);
+
+    // Adds a vertex to the set of functions
+    void addVertexFinding(valFindPrimaryVertex,valFindDecayVertex){
+
+        fFindPrimaryVertex=valFindPrimaryVertex;
+        fFindDecayVertex=valFindDecayVertex;
+
+    };
+
+    void addVertexFitting();
+
+    void add3CFit();
 
 };
 
