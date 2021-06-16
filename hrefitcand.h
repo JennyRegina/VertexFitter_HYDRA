f87b0f93d637d62bec5b44599317412004aab284
Jana Rieger <j.rieger@gsi.de>
2021-06-16 15:59:47 +0200

    add IsForward flag to hrefitcand.h

--- hrefitcand.h
+++ hrefitcand.h
@@ -12,6 +12,7 @@ class HRefitCand : public TLorentzVector {
 private:
     HVirtualCand * cand;
     Double_t fR, fZ; //fTheta, fPhi;
+    Bool_t fIsForward = false;
     TMatrixD fCov;
 
 public:
@@ -20,12 +21,14 @@ public:
     ~HRefitCand(){};
     void setR(Double_t val) { fR = val; }
     void setZ(Double_t val) { fZ = val; }
+    void setIsForward(Bool_t val) {fIsForward = val; }
     //void setTheta(Double_t val) { fTheta = val; }
     //void setPhi(Double_t val) { fPhi = val; }
 
     void setCovariance(const TMatrixD & cov);
     Double_t getR() const { return fR; }
     Double_t getZ() const { return fZ; }
+    Bool_t getIsForward() const { return fIsForward; }
     //Double_t getTheta() const { return fTheta; }
     //Double_t getPhi() const { return fPhi; }
     TMatrixD getCovariance() const {return fCov; }
