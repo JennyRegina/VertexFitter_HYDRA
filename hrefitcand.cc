e3f69f2016a8fe97654801375ef0152e13b98a66
Jenny Regina <jenny.regina@physics.uu.se>
2021-08-19 11:40:36 +0200

    Added angular functions and fIsUsed flag

--- hrefitcand.cc
+++ hrefitcand.cc
@@ -1,10 +1,8 @@
 // ROOT includes
 #include "hrefitcand.h"
 
-//TLorentzVector(*cand), fR(cand->getR()), fZ(cand->getZ()), fTheta(cand->Theta()), fPhi(cand->Phi()), cand(cand)
-
 HRefitCand::HRefitCand(HVirtualCand* cand)
-    : TLorentzVector(*cand), cand(cand), fR(cand->getR()), fZ(cand->getZ()), fIsForward(false)
+    : TLorentzVector(*cand), cand(cand), fTheta(cand->Theta()), fPhi(cand->Phi()), fR(cand->getR()), fZ(cand->getZ()), fIsForward(false)
 {
 }
 
@@ -12,8 +10,8 @@ HRefitCand::HRefitCand()
     : TLorentzVector(), fIsForward(false)
 {
     cand = new HVirtualCand();
-    fR = cand->getR();
-    fZ = cand->getZ();
+    //fR = cand->getR();
+    //fZ = cand->getZ();
 }
 
 void HRefitCand::setCovariance(const TMatrixD& cov)
--- hrefitcand.h
+++ hrefitcand.h
@@ -7,31 +7,33 @@
 #include <TLorentzVector.h>
 #include <hvirtualcand.h>
 
-
-class HRefitCand : public TLorentzVector {
+class HRefitCand : public TLorentzVector
+{
 private:
-    HVirtualCand * cand;
-    Double_t fR, fZ; //fTheta, fPhi;
-    Bool_t fIsForward;
+    HVirtualCand *cand;
+    Double_t fR, fZ, fTheta, fPhi;
+    Bool_t fIsForward, fIsUsed;
     TMatrixD fCov;
 
 public:
-    HRefitCand(HVirtualCand * cand);
+    HRefitCand(HVirtualCand *cand);
     HRefitCand(); //Does that work like this? See https://os.mbed.com/users/fpucher/code/HIM0Board/wiki/Vererbung-in-C%2B%2B
     ~HRefitCand(){};
     void setR(Double_t val) { fR = val; }
     void setZ(Double_t val) { fZ = val; }
-    void setIsForward(Bool_t val) {fIsForward = val; }
-    //void setTheta(Double_t val) { fTheta = val; }
-    //void setPhi(Double_t val) { fPhi = val; }
+    void setIsUsed(Double_t val) { fIsUsed = val; }
+    void setIsForward(Bool_t val) { fIsForward = val; }
+    void setTheta(Double_t val) { fTheta = val; }
+    void setPhi(Double_t val) { fPhi = val; }
 
-    void setCovariance(const TMatrixD & cov);
+    void setCovariance(const TMatrixD &cov);
     Double_t getR() const { return fR; }
     Double_t getZ() const { return fZ; }
+    Double_t getTheta() const { return fTheta; }
+    Double_t getPhi() const { return fPhi; }
     Bool_t getIsForward() const { return fIsForward; }
-    //Double_t getTheta() const { return fTheta; }
-    //Double_t getPhi() const { return fPhi; }
-    TMatrixD getCovariance() const {return fCov; }
+    Bool_t getIsUsed() const { return fIsUsed; };
+    TMatrixD getCovariance() const { return fCov; }
 
     void reset();
     void update();
