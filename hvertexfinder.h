22cc7c14928bf095688068db08d6133363550e57
Jenny Regina <jenny.regina@physics.uu.se>
2021-09-06 16:49:09 +0200

    Variable number of input particles to vertex finding

--- hvertexfinder.h
+++ hvertexfinder.h
@@ -23,36 +23,15 @@ using std::cout;
 using std::endl;
 
 class HVertexFinder
-
- 
 {
+
 private:
     std::vector<HRefitCand> fCands;
+    //std::vector<int> fPids; // Index of a Pid in this vector correspond to that of the cand on the corresponding index in fCands.
+    //std::vector<double> fWeight;
 
     TVector3 fVertex;
-    TVector3 fPrimaryVertex;
     int fVerbose;
-
-    double fDistanceParticleToParticle;
-    double fDistanceParticleToVertex;
-
-    double fDistParticle1Vertex;
-    double fDistParticle2Vertex;
-    double fDistParticle1Origin;
-    double fDistParticle2Origin;
-
-    bool fPrimaryVertexFound;
-
-    // Properties related to difference between primary and decay vertex
-    TVector3 fVecPrimToDecayVertex;
-    double fDistPrimToDecayVertex;
-
-    // Checks if the decay vertex is located further downstream in z-position
-    // and at larger values of R compared to the primary vertex
-    bool fPrimaryVertexIsBetforeDecayVertex;
-    bool fPrimaryVertexIsInsideDecayVertex;
-
-    bool fUsePrimaryVertexInNeutralCandidateCalculation;
 
 public:
     HVertexFinder();
@@ -61,26 +40,9 @@ public:
     void setVerbosity(int val) { fVerbose = val; }
 
     TVector3 findVertex(const std::vector<HRefitCand> &cands);
-    TVector3 findPrimaryVertex(const std::vector<HRefitCand> &cands);
-    std::vector<HRefitCand> UpdateTrackParameters(std::vector<HRefitCand> &cands, TVector3 &VertexPos);
-    void calculateVertexProperties(TVector3 primaryVertex, TVector3 decayVertex);
 
-    TVector3 getVertex() const { return fVertex; }               // Function that the user should use in the analysis macro
-    TVector3 gePrimarytVertex() const { return fPrimaryVertex; } // Function that the user should use in the analysis macro
+    TVector3 getVertex() const { return fVertex; }
 
-    double getDistanceBetweenFittedParticles() const { return fDistanceParticleToParticle; }
-    double getDistanceFirstParticleVertex() const { return fDistParticle1Vertex; }
-    double getDistanceSecondParticleVertex() const { return fDistParticle2Vertex; }
-    double getDistanceFirstParticleOrigin() const { return fDistParticle1Origin; }
-    double getDistanceSecondParticleOrigin() const { return fDistParticle2Origin; }
-
-    double getDistBetweenVertices() { return fDistPrimToDecayVertex; }
-
-    bool isPrimVertexBeforeDecayVertex() { return fPrimaryVertexIsBetforeDecayVertex; }
-    bool isPrimVertexInsideDecayVertex() { return fPrimaryVertexIsInsideDecayVertex; }
-
-    TVector3 getPrimaryVertex() { return fPrimaryVertex; }
-    
 };
 
 #endif /* HVERTEXFINDER_H */
